627dfb4d3f1d0d094cb2c93846b765fcadfee705
Hicran Apaydın <hicran.apaydn@gmail.com>
2025-07-21 23:23:07 +0300

    feat: gh pages settings

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,15 @@
 import type { NextConfig } from "next";
 
+// next.config.js
+
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
+  output: "export",
+  basePath: isProd ? "/nextjs-retro-console-portfolio" : "",
+  assetPrefix: isProd ? "/nextjs-retro-console-portfolio/" : "",
+};
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
