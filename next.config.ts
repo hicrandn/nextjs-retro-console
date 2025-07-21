86b5ba175f79a108e917d444c3ecbb9e09843960
Hicran Apaydın <hicran.apaydn@gmail.com>
2025-07-21 23:29:34 +0300

    fix: ES6 export default

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,11 @@
 import type { NextConfig } from "next";
 
-// next.config.js
-
 const isProd = process.env.NODE_ENV === "production";
 
-module.exports = {
+const nextConfig: NextConfig = {
   output: "export",
   basePath: isProd ? "/nextjs-retro-console-portfolio" : "",
   assetPrefix: isProd ? "/nextjs-retro-console-portfolio/" : "",
-};
-
-const nextConfig: NextConfig = {
-  /* config options here */
 };
 
 export default nextConfig;
